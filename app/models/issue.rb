24d9fdb6611fb4ea85bf90cd1367134b4434934a
Jeremy Kratz <jwkratz@gmail.com>
2015-02-20 17:25:25 -0600

    Added issue index view; added issue-person relationships

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,4 +2,8 @@ class Issue < ActiveRecord::Base
   belongs_to :project
   belongs_to :priority
   belongs_to :status
+  has_one :fixer, :class_name => "Person", :foreign_key => "id"
+  has_one :tester, :class_name => "Person", :foreign_key => "id"
+  has_one :creator, :class_name => "Person", :foreign_key => "id"
+  has_one :last_updater, :class_name => "Person", :foreign_key => "id"
 end
