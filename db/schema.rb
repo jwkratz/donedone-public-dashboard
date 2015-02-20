32d60e957f61e526c009b47bacd6ac3f42b3cc03
Jeremy Kratz <jwkratz@gmail.com>
2015-02-20 16:11:34 -0600

    Added Person model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219210008) do
+ActiveRecord::Schema.define(version: 20150220221106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
